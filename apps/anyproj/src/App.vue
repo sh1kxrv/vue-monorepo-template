70c2c691cbdc456fceebe0210653b135d79d064c
Egor <business.xaf@gmail.com>
2025-05-24 05:12:58 +0600

    chore: minor changes

--- apps/anyproj/src/App.vue
+++ apps/anyproj/src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { RouterView, useRoute } from 'vue-router'
 import LayoutDefault from '#/layouts/layout.default.vue'
+
+import { RouterView, useRoute } from 'vue-router'
+import { computed } from 'vue'
 
 const route = useRoute()
 
