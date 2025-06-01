b9f8acccd80b028b3c050bccfe94cf8f90b35eb9
Egor <business.xaf@gmail.com>
2025-06-01 19:15:44 +0600

    fix: tailwindcss vite plugin

--- internal/vite-config/src/plugins/application.ts
+++ internal/vite-config/src/plugins/application.ts
@@ -1,44 +1,45 @@
-import type { PluginOption } from 'vite';
+import type { PluginOption } from 'vite'
 
-import type { ApplicationPluginOptions } from '../types/config';
+import type { ApplicationPluginOptions } from '../types/config'
 
-import viteCompressPlugin from 'vite-plugin-compression';
+import viteCompressPlugin from 'vite-plugin-compression'
 
-import { loadConditionPlugins } from '.';
-import { loadCommonPlugins } from './common';
-import { useIsolatedAlias } from './plugin/vite.isolated-alias';
+import { loadConditionPlugins } from '.'
+import { loadCommonPlugins } from './common'
+import { useIsolatedAlias } from './plugin/vite.isolated-alias'
+import tailwindcss from '@tailwindcss/vite'
 
 export async function loadApplicationPlugins(
   options: ApplicationPluginOptions,
 ): Promise<PluginOption[]> {
-  const isBuild = options.isBuild;
+  const isBuild = options.isBuild
 
-  const { compress, compressTypes, ...commonOptions } = options;
+  const { compress, compressTypes, ...commonOptions } = options
 
-  const commonPlugins = await loadCommonPlugins(commonOptions);
+  const commonPlugins = await loadCommonPlugins(commonOptions)
 
   return await loadConditionPlugins([
     ...commonPlugins,
     {
       condition: true,
-      plugins: () => [useIsolatedAlias()],
+      plugins: () => [useIsolatedAlias(), tailwindcss()],
     },
     {
       condition: isBuild && !!compress,
       plugins: () => {
-        const compressPlugins: PluginOption[] = [];
+        const compressPlugins: PluginOption[] = []
         if (compressTypes?.includes('brotli')) {
           compressPlugins.push(
             viteCompressPlugin({ deleteOriginFile: false, ext: '.br' }),
-          );
+          )
         }
         if (compressTypes?.includes('gzip')) {
           compressPlugins.push(
             viteCompressPlugin({ deleteOriginFile: false, ext: '.gz' }),
-          );
+          )
         }
-        return compressPlugins;
+        return compressPlugins
       },
     },
-  ]);
+  ])
 }
