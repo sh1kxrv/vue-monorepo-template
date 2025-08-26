780bfd3732a4342f4926408abcc77a3df59b1da3
Egor <ItsAwware@yandex.ru>
2025-08-26 15:00:26 +0500

    feat: date formatter & currency

--- pkgs/shared/src/utils/index.ts
+++ pkgs/shared/src/utils/index.ts
@@ -1,1 +1,14 @@
-export * from './middleware';
+export * from "./middleware";
+
+export function createFormatter() {
+  return new Intl.NumberFormat("ru-RU", {
+    maximumFractionDigits: 2,
+  });
+}
+
+export function createDateFormatter(options: Intl.DateTimeFormatOptions) {
+  return new Intl.DateTimeFormat("ru-RU", options);
+}
+
+export const capitalize = (s: string) =>
+  s && String(s[0]).toUpperCase() + String(s).slice(1);
